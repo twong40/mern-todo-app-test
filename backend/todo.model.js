ff188ad5dcdbd5f94033914b1c51e8e27b972c91
thomaswong1000@gmail.com <twong40@ucsc.edu>
2019-11-07 21:34:36 -0800

    fixed folder problem

---
+++ backend/todo.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Scheme = mongoose.Schema;
+
+let Todo = new Scheme({
+	todo_description: {
+		type: String
+	},
+	todo_responsible: {
+		type: String
+	},
+	todo_priority: {
+		type: String
+	},
+	todo_completed: {
+		type: Boolean
+	}
+});
+
+module.exports = mongoose.model('Todo', Todo);
